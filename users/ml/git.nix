f5cc290ad3dda6eb043470a931df4f8a46d17420
Moritz Lumme <46034439+m-lumme@users.noreply.github.com>
2021-12-19 23:31:15 +0100

    git: port configuration from dotfiles to home-manager

--- users/ml/git.nix
+++ users/ml/git.nix
@@ -4,6 +4,25 @@
 {
   programs.git = {
     enable = true;
-
+    userName = "Moritz Lumme";
+    userEmail = "46034439+m-lumme@users.noreply.github.com";
+    extraConfig = {
+      core = {
+        editor = "vim";
+      };
+      color = {
+        ui = "auto";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      diff = {
+        tool = "vim -d";
+      };
+      merge = {
+        tool = "vim -d";
+      };
+    };
   };
 }
+
