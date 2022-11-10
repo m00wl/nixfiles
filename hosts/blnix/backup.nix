cc79d639685bd2a8d647bd4d14a589516fac029a
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2022-11-10 13:17:33 +0100

    [blnix]: add borg repo for slnix backup

--- hosts/blnix/backup.nix
+++ hosts/blnix/backup.nix
@@ -2,4 +2,13 @@
 
 {
   # Define backup here.
+  services.borgbackup.repos = {
+    slnix = {
+      path = "/srv/backup/slnix";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICDE1KJeqyaECQKZi3RcigLXOWKeIKG6yCnSZ9Oj/FJR backup-slnix"
+      ];
+    };
+  };
+
 }
