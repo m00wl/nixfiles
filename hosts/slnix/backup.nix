309d9774cab8dfb5ad7f68f017a67d0df1bc1d27
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2022-11-10 16:43:20 +0100

    [slnix]: switch data backups to blnix

--- hosts/slnix/backup.nix
+++ hosts/slnix/backup.nix
@@ -1,16 +1,40 @@
 { config, pkgs, ... }:
 
+let
+  cfg = config.services.borgbackup;
+in
 {
-  # Create backup user account
-  users.users.backup = {
-    isSystemUser = true;
-    group = "backup";
-    extraGroups = [ "users" ];
-    useDefaultShell = true;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDMZ8aIbzb709hde3ggnTito4Y5QdXwce6VnekIBPap backup@slnix"
-    ];
+  sops.secrets = {
+    "backup/ssh-key" = {
+      sopsFile = ./secrets.yaml;
+      owner = cfg.jobs.data.user;
+      group = cfg.jobs.data.group;
+    };
+    "backup/repo-pwd" = {
+      sopsFile = ./secrets.yaml;
+      owner = cfg.jobs.data.user;
+      group = cfg.jobs.data.group;
+    };
   };
 
-  users.groups.backup = {};
+  services.borgbackup.jobs = {
+    data = {
+      paths = "/data";
+      environment.BORG_RSH = "ssh -i /run/secrets/backup/ssh-key";
+      repo = "borg@backup.lumme.de:/srv/backup/slnix";
+      encryption = {
+        mode = "repokey";
+        passCommand = "cat /run/secrets/backup/repo-pwd";
+      };
+      compression = "auto,lzma";
+      startAt = "*-*-* 04:00:00";
+      prune.keep = {
+        within = "1d"; # Keep all archives from the last day
+        daily = 7;
+        weekly = 4;
+        monthly = -1;  # Keep at least one archive for each month
+      };
+      persistentTimer = true;
+    };
+  };
 }
