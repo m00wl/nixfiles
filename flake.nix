053b8dd9b0d7cd92df892e2a305c8b142bf8a610
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2023-12-04 22:31:45 +0100

    update NixOS to version 23.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "m00wl's nixfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
