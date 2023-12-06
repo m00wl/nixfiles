157bc65f78f5792564a857fff7c37437bc048848
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2023-12-06 13:02:19 +0100

    switch nixpkgs input to the 'nixos-*' branch
    This ensures better availability of pre-built derivations in the binary
    cache.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "m00wl's nixfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
