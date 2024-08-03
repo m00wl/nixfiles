f69cb7e258035f4148d1bf30265af26848bb568d
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2024-08-03 12:44:21 +0200

    update NixOS to version 24.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "m00wl's nixfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
