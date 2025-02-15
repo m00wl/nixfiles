0fa3909eac3f033c35f76652f470f3a0b7502a6b
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2025-02-15 21:35:52 +0100

    switch locale and keyboard layout

--- modules/src/i18n.nix
+++ modules/src/i18n.nix
@@ -1,8 +1,5 @@
 {
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "de";
-  };
+  # english locale with sane date format.
+  i18n.defaultLocale = "en_DK.UTF-8";
+  console.font = "Lat2-Terminus16";
 }
