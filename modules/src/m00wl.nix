7f180fc10fcb5fe08ede73ae3e6ff6a1c936a555
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2025-04-13 23:13:21 +0200

    extract ssh pub key to own file

--- modules/src/m00wl.nix
+++ modules/src/m00wl.nix
@@ -4,8 +4,8 @@
   users.users.m00wl = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/9QLlB+fy5eSjqEQvIznnPxZETamnnKLWBoXpZeLLG me@moritz.lumme.de"
+    openssh.authorizedKeys.keyFiles = [
+      ../../users/m00wl/id_ed25519_mwl.pub
     ];
   };
 }
